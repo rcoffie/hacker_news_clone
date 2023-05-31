16b4bae1e71b1df1324ada9c1ddc4e9c8d961c00
Robert Coffie <rcoffie22@yhahoo.com>
2023-05-31 09:28:31 +0000

    adding login and register view

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from pages import views 
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
    path('', views.home, name='home'), 
+   path('signup/',views.signup,name="signup"),
+   path('login/',LoginView.as_view(template_name="pages/login.html"), name="login"),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 # Create your views here.
 
 def home(request):
 
     return render(request, 'pages/homepage.html')
+
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'pages/signup.html', {'form':form,})
