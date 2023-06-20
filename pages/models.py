2d380e1e3ff1a33b9a53ebc0473881250ae7e706
Robert Coffie <rcoffie22@yahoo.com>
2023-06-20 15:08:50 +0000

    updating userprofile

--- pages/models.py
+++ pages/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=100, blank=True, null=True)
-    profile_picture = models.ImageField(upload_to="profile_picture", null=True, blank=True)
+    profile_picture = models.ImageField(upload_to="profile_picture", null=True, blank=True, default="default_profile.jpg")
     about = models.TextField(blank=True, null=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
