4d832c2a1c33df619d443afa7df9f8cda03ac6fd
Robert Coffie <rcoffie22@yahoo.com>
2023-06-21 09:21:09 +0000

    refacotring pages app

--- story/forms.py
+++ story/forms.py
@@ -1,28 +1,24 @@
 from django import forms
 from django.forms import ModelForm
-from story.models import Story, Comment
 
+from story.models import Comment, Story
 
 
 class StoryForm(ModelForm):
-    
     class Meta:
-        model = Story 
-        fields = ('title','url','text')
+        model = Story
+        fields = ("title", "url", "text")
         widgets = {
-            'title':forms.TextInput(attrs={'class':'form-control','placeholder':'Post Title'}),
-            'url': forms.TextInput(attrs={'class':'form-control'}),
-            'text':forms.Textarea(attrs={'class':'form-control'}),
+            "title": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "Post Title"}
+            ),
+            "url": forms.TextInput(attrs={"class": "form-control"}),
+            "text": forms.Textarea(attrs={"class": "form-control"}),
         }
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
-        fields = ('text',)
-        widgets = {
-            
-            'text':forms.Textarea(attrs={'class':'form-control'})
-        }
-
-        
-  
+        fields = ("text",)
+        widgets = {"text": forms.Textarea(attrs={"class": "form-control"})}
--- story/views.py
+++ story/views.py
@@ -1,60 +1,68 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from story.forms import StoryForm, CommentForm
-from story.models import Story, Comment, Vote
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect, render
+
+from story.forms import CommentForm, StoryForm
+from story.models import Comment, Story, Vote
+
 # Create your views here.
+
 
 @login_required
 def create_story(request):
-    form = StoryForm() 
+    form = StoryForm()
     if request.method == "POST":
         form = StoryForm(request.POST)
         if form.is_valid():
             story = form.save(commit=False)
-            story.created_by = request.user 
+            story.created_by = request.user
             story.save()
-            return redirect('home')
+            return redirect("home")
     else:
         form = StoryForm()
 
-    return render(request, 'story/create_story.html', {'form':form})
+    return render(request, "story/create_story.html", {"form": form})
 
 
 def story_detail(request, id):
     story = Story.objects.get(id=id)
     story.has_voted = False
     if request.user.is_authenticated:
-         if story.votes.filter(created_by =request.user):
-             story.has_voted = True
+        if story.votes.filter(created_by=request.user):
+            story.has_voted = True
     comments = Comment.objects.filter(story=story)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.story = story
-            comment.created_by = request.user 
+            comment.created_by = request.user
             comment.save()
-            return redirect('detail', id=id)
+            return redirect("detail", id=id)
     else:
         form = CommentForm()
-    return render(request, 'story/story_detail.html',{'story':story,'form':form,'comments':comments,})
+    return render(
+        request,
+        "story/story_detail.html",
+        {
+            "story": story,
+            "form": form,
+            "comments": comments,
+        },
+    )
 
 
 def vote(request, id):
     story = Story.objects.get(id=id)
-    already_voted = story.votes.filter(created_by=request.user )
+    already_voted = story.votes.filter(created_by=request.user)
     if not already_voted:
         Vote.objects.create(story=story, created_by=request.user)
     else:
         vote = Vote.objects.get(story_id=id, created_by=request.user)
         vote.delete()
 
-    redirect_page = request.GET.get('redirect_page', '/')
+    redirect_page = request.GET.get("redirect_page", "/")
 
     if redirect_page == "detail":
         return redirect("detail", id=id)
     else:
         return redirect(redirect_page)
-
-
-
